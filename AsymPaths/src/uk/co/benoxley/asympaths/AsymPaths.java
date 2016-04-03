983fe3de84505ff3fb653c563e20780f93f585e2
Ben Oxley <ben.oxley@gmail.com>
2016-04-03 23:32:18 +0100

    Updated after re-reading test2

--- AsymPaths/src/uk/co/benoxley/asympaths/AsymPaths.java
+++ AsymPaths/src/uk/co/benoxley/asympaths/AsymPaths.java
@@ -7,6 +7,7 @@ package uk.co.benoxley.asympaths;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,7 +29,7 @@ public class AsymPaths {
             System.out.println(new File("").getAbsolutePath());
             List<Point> nodes = CSVReader.readNodes(new File(nodesFile));
             AsymPaths.runAnalyser(mapFile,nodes.get(0),nodes.get(1),"Test1.txt");
-            AsymPaths.runAnalyser(mapFile,nodes.get(0),nodes.get(nodes.size()-1),"Test2.txt");
+            AsymPaths.runAnalyser(mapFile,nodes,"Test2.txt");
             int[][] map = CSVReader.readMap(new File(mapFile)); 
             Tree tree = MinSpanningTree.BoruvkaAlgorithm(nodes, map);
             System.out.println("Number of edges in tree: "+tree.edges.size());
@@ -46,6 +47,27 @@ public class AsymPaths {
             List<Point> findRoute = solver.findRoute(start, end, (a,b)->0);
             System.out.println(findRoute);
             CSVReader.writeNodes(new File(outFile), findRoute);
+        } catch (IOException ex) {
+            Logger.getLogger(AsymPaths.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void runAnalyser(String mapFile, List<Point> points, String outFile){
+        try {
+            int[][] map = CSVReader.readMap(new File(mapFile));           
+            Solver solver = new Solver(map);
+            int pointer = 0;
+            List<Point> fullList = new ArrayList<>();
+            while (pointer <=points.size()-2){
+                List<Point> findRoute = solver.findRoute(points.get(pointer), points.get(pointer+1), (a,b)->0);
+                if (fullList.size()>0&&fullList.get(fullList.size()-1).equals(findRoute.get(findRoute.size()-1))){
+                    fullList.remove(fullList.get(fullList.size()-1));
+                }
+                fullList.addAll(findRoute);
+            }
+            
+            
+            CSVReader.writeNodes(new File(outFile), fullList);
         } catch (IOException ex) {
             Logger.getLogger(AsymPaths.class.getName()).log(Level.SEVERE, null, ex);
         }
